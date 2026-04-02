f335659810a6b9b269f8b02a4dcf6fc763fbc918
Bern Francis <162781071+butawanbern@users.noreply.github.com>
2026-04-02 20:52:44 +0800

    Remove Keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from "astro/config";
-import keystatic from "@keystatic/astro";
-import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel(),
-  integrations: [keystatic()],
+  output: "static",
 });
